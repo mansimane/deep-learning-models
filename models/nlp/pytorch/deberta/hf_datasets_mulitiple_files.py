65d607add5ea283da09279e5539edcd1b3df4546
Mansi Mane <mansmane@amazon.com>
2021-01-27 18:06:53 -0800

    Updated code to read dataset from multiple files

--- models/nlp/pytorch/deberta/hf_datasets_mulitiple_files.py
+++ models/nlp/pytorch/deberta/hf_datasets_mulitiple_files.py
@@ -29,8 +29,10 @@ class MyIterableDataset(IterableDataset):
         # filenames = [os.path.join(file_path, filename) for filename in os.listdir(file_path)]
         # for filename in filenames:
 
-        skip_nexline = True
+        skip_nexline = False
+        print(file_path)
         with open(file_path, 'r') as file_obj:
+
             for line in file_obj:
                 if not skip_nexline:
                     if not "<doc id=" in line and not "</doc>" in line:
@@ -39,32 +41,71 @@ class MyIterableDataset(IterableDataset):
                                                             add_special_tokens=True,
                                                             truncation=True,
                                                             max_length=self.block_size)
-                            yield {"input_ids": torch.tensor(batch_encoding["input_ids"][0], dtype=torch.long)}
+                            # yield {"input_ids": torch.tensor(batch_encoding["input_ids"][0], dtype=torch.long)}
+                            yield line
                 if "<doc id=" in line:
                     # tile after  doc is usually title
                     skip_nexline = True
                 else:
                     skip_nexline = False
+
     def get_steam(self,file_list):
         return chain.from_iterable(map(self.parse_file, file_list))
 
     def __iter__(self):
         return self.get_steam(self.filenames)
 
-iterable_dataset = MyIterableDataset('/fsx/data/wiki_test_dir',
+iterable_dataset = MyIterableDataset('/fsx/data/wikidemo',
                                      tokenizer)
-
-train_dataset = LineByLineTextDataset(
-    tokenizer=tokenizer,
-    file_path="/fsx/data/wikidemo/wiki_test",
-    block_size=128,
-)
+#
+# train_dataset = LineByLineTextDataset(
+#     tokenizer=tokenizer,
+#     file_path="/fsx/data/wikidemo/wiki_test",
+#     block_size=128,
+# )
 
 # for example in train_dataset:
 #     print(example)
 #
-for i in iterable_dataset:
-    print(i)
-    if i == 20:
-        break
+# for i in iterable_dataset:
+#     print(i)
+#     if i == 100:
+#         break
 
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, mlm=True, mlm_probability=0.15
+)
+config = DebertaConfig()
+
+model = DebertaForMaskedLM(config=config)
+
+training_args = TrainingArguments(
+    output_dir="./deberta",
+    overwrite_output_dir=True,
+
+    num_train_epochs=1000,
+    per_gpu_train_batch_size=32,
+    learning_rate=5e-10,
+    weight_decay=0.01,
+    adam_beta1=0.9,
+    adam_beta2=0.999,
+    adam_epsilon=1e06,
+    max_grad_norm=1.0,
+    save_steps=10_000,
+    save_total_limit=2,
+    logging_first_step=False,
+    logging_steps=1,
+    max_steps=10000,
+    gradient_accumulation_steps=10,
+
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=iterable_dataset,
+)
+
+print("Starting training")
+trainer.train()
